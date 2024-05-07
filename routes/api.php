c71f32c4e3f1f7a85efaa2b18ea4d99855349a5c
Gustavo Caetano <souljacker@gmail.com>
2024-05-07 17:09:59 -0300

    Add services and appointments endpoints to api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\MedspaServicesController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,18 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('{medspa}/appointments', [AppointmentController::class, 'store']);
         Route::get('{medspa}/services', MedspaServicesController::class);
 
+    });
+
+    Route::prefix('services')->group(function () {
+        Route::get('{service}', [ServiceController::class, 'show']);
+        Route::post('/', [ServiceController::class, 'store']);
+        Route::put('/{service}', [ServiceController::class, 'update']);
+    });
+
+    Route::prefix('appointments')->group(function () {
+        Route::get('/', [AppointmentController::class, 'index']);
+        Route::get('{appointment}', [AppointmentController::class, 'show']);
+        Route::patch('{appointment}', [AppointmentController::class, 'update']);
     });
 
 });
